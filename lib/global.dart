4bae672c74c452c8088c7e3037962264771150be
mhborgatto <mhborgatto@gmail.com>
2020-02-20 10:28:51 -0300

    Inclusao de icones

---
+++ lib/global.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+double fontsize = 20;
+double heightsizedbox = 20;
+
+Color colorlabelstyle = Colors.black26;
+Color colortextstyle = Colors.black54;
+Color iconcolor = Colors.black;
+Color labelstlyle = Colors.black26;
+Color globalcolor = Colors.white;
+
+
+
--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
-
-/// Construct a color from a hex code string, of the format #RRGGBB.
-Color hexToColor(String code) {
-  return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
-}
+import 'package:flutterlayout/global.dart' as global;
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
+    return MaterialApp(
+      home:Scaffold(
+        backgroundColor:global.globalcolor,
+        body: Container(
           padding: EdgeInsets.only(
-            top: 60,
+            top: 20,
             left: 40,
             right: 40,
           ),
-          color: Colors.white10,
           child: ListView(
             children: <Widget>[
               SizedBox(
@@ -23,21 +20,35 @@ class LoginPage extends StatelessWidget {
                 height: 128,
                 child: Image.asset("assets/logo.png"),
               ),
-              SizedBox(height: 20,
+              Divider(),
+              Text(
+                "Força de Vendas",
+                style: TextStyle(
+                  fontFamily: 'Lobster',
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(
+                height: global.heightsizedbox,
               ),
               TextFormField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.business),
                   labelText: "CNPJ",
+                  hintText: "00.000.000/0000-00",
+                  border: OutlineInputBorder(),
                   labelStyle: TextStyle(
-                    color: Colors.black26,
+                    color: global.colorlabelstyle,
                     fontWeight: FontWeight.w400,
-                    fontSize:20,
+                    fontSize: global.fontsize,
                   ),
                 ),
                 style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black54,
+                  fontSize: global.fontsize,
+                  color: global.colortextstyle,
                 ),
               ),
               SizedBox(
@@ -46,36 +57,40 @@ class LoginPage extends StatelessWidget {
               TextFormField(
                 keyboardType: TextInputType.text,
                 decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.people, color: Colors.red),
                   labelText: "Usuário",
                   labelStyle: TextStyle(
                     color: Colors.black26,
                     fontWeight: FontWeight.w400,
-                    fontSize:20,
+                    fontSize: global.fontsize,
                   ),
                 ),
                 style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black54,
+                  fontSize: global.fontsize,
+                  color: global.colortextstyle,
                 ),
               ),
               SizedBox(
                 height: 10,
-              ),              
+              ),
               TextFormField(
                 keyboardType: TextInputType.text,
                 obscureText: true,
                 decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.vpn_key),
                   labelText: "Senha",
                   labelStyle: TextStyle(
-                    color: Colors.black26,
+                    color: global.colorlabelstyle,
                     fontWeight: FontWeight.w400,
-                    fontSize: 20,
+                    fontSize: global.fontsize,
                   ),
-                 ),
-                 style: TextStyle(
-                   fontSize: 20,
-                   color:Colors.black54,
-                 ),
+                ),
+                style: TextStyle(
+                  fontSize: global.fontsize,
+                  color: global.colortextstyle,
+                ),
               ),
               Container(
                 height: 40,
@@ -95,13 +110,13 @@ class LoginPage extends StatelessWidget {
                 height: 20,
               ),
               Container(
-                height: 60,  
+                height: 60,
                 alignment: Alignment.centerLeft,
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
                     begin: Alignment.topLeft,
                     end: Alignment.bottomRight,
-                    stops: [0.3,1],
+                    stops: [0.3, 1],
                     colors: [
                       Color(0xFFff6659),
                       Color(0xFF9a0007),
@@ -139,7 +154,8 @@ class LoginPage extends StatelessWidget {
               ),
             ],
           ),
-      ),
+        ),
+      )
     );
   }
 }
